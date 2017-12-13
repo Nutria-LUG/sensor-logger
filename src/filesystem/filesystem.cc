353d4af40220c2aa769cfd4abe7244532c33f9ad
glabita <glabita@localhost.localdomain>
2017-12-13 14:02:14 +0100

    fix: Make program work on Debian 8.8.0

--- src/filesystem/filesystem.cc
+++ src/filesystem/filesystem.cc
@@ -1,8 +1,4 @@
-#include <string>
-#include <experimental/filesystem>
 #include "./filesystem.hh"
-
-namespace fs = std::experimental::filesystem;
 
 Filesystem::Filesystem() { }
 Filesystem::~Filesystem() { }
